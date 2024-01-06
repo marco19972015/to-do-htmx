250cdb8c02947f3d2ba2146148752d0c93c0d924
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-06 18:20:13 -0500

    In this version, I removed the initial path that was set to get the JSON array of users and instead it was replaced with /users so it can look in our own server. From there I configure the server.js file to handle a GET request to fetch users. I start this off by creating a list of static users that we want to return from the route we created in index.html. Then using the response parameter, I use the send method and add back ticks so we can return a template string. Initiialy I set to return an array (because of the use of map opperator), but by adding .join and empty string we return that array in a string format.

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// Handle GET request to fetch users 
+// Create list of static users that we want to return from this route
+app.get('/users', (req, res) => {
+    const users = [
+        {id: 1, name: 'John Doe'},
+        {id: 2, name: 'Bob Williams'},
+        {id: 3, name: 'Eddy Ed'},
+    ];
+
+    // use the res prop (contains the send method), and add back-tiques so it can be a template string
+    // Initially I return an array (map), but adding .join and empty string returns us a string
+    res.send(`
+        <h1 class="text-2xl font-bold my-4">Users</h1>
+        <ul>
+            ${users.map((user) => `<li>${user.name}</li>`).join('')}
+        </ul>
+    `)
+});
+
 // Start the server
 app.listen(3000, () => {
     console.log(`Server listening on port 3000`);
